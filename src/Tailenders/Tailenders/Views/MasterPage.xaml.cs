852f5ef8633ef8975ea3b571a9cb2c5a571dc2a3
Aden Earnshaw <aearnshaw@outlook.com>
2018-10-17 20:32:38 +0100

    Adding MasterDetailPage

---
+++ src/Tailenders/Tailenders/Views/MasterPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Tailenders.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MasterPage : MasterDetailPage
+    {
+        public MasterPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
